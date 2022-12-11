21b65962016b18b765ae92b6efadb09be9e78e3c
KrisjanisP <krisjanispetrucena@gmail.com>
2022-12-11 22:55:27 +0200

    complete homework

---
+++ cpp/1-praktiskais/string.h
@@ -0,0 +1,25 @@
+#include <vector>
+#include <cstring>
+#include <string>
+
+namespace kpetrucena {
+    // my own string implementation
+    // saved as vector of char arrays
+    class string {
+    private:
+        std::vector<char*> data;
+        std::vector<int> length;
+    public:
+        string();
+        string(const char str[]);
+        ~string();
+        std::string toSTDString() const;
+        int size() const;
+        char* operator[](int i);
+    };
+}
+
+std::istream& operator>>(std::istream& is, kpetrucena::string& str);
+std::ostream& operator<<(std::ostream& os, const kpetrucena::string& str);
+
+std::istream& getline(std::istream& is, kpetrucena::string& str);
