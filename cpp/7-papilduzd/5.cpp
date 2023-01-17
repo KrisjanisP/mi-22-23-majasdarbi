4895ec6103bc289b0fb80571acf5a5bde1b00003
KrisjanisP <krisjanispetrucena@gmail.com>
2023-01-18 00:27:32 +0200

    add ui

--- cpp/7-papilduzd/5.cpp
+++ cpp/7-papilduzd/5.cpp
@@ -33,22 +33,65 @@ struct Stack
     }
 };
 
+enum class eAction
+{
+    AddElement = 1,
+    PopElement = 2,
+    Empty = 3,
+    Exit = 4,
+};
+
+eAction promptAction()
+{
+    std::pair<eAction, std::string> actions[] = {
+        {eAction::AddElement, "Ievietot jaunu elementu"},
+        {eAction::PopElement, "Izņemt elementu no steka"},
+        {eAction::Empty, "Pārbaudīt, vai steks ir tukšs"},
+        {eAction::Exit, "Beigt Programmu"}};
+
+    cout << "Izvēlieties darbību:\n";
+    for (auto action : actions)
+    {
+        cout << "\t" << (int)action.first << ". " << action.second << endl;
+    }
+
+    eAction result;
+    input("Izvēlieties darbību [1-4]: ", (int &)result, [](int x) -> bool
+          { return x >= 1 && x <= 4; });
+    return result;
+}
+
 int main()
 {
     cout << "5. UZDEVUMS" << endl;
     Stack stack;
-    if (stack.empty())
-        cout << "ir tukšs" << endl;
-    else
-        cout << "nav tukšs" << endl;
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    cout << stack.pop() << endl;
-    cout << stack.pop() << endl;
-    cout << stack.pop() << endl;
-    if (stack.empty())
-        cout << "ir tukšs" << endl;
-    else
-        cout << "nav tukšs" << endl;
+    while(true) {
+        eAction action = promptAction();
+        // switch and use stack depending on action
+        switch (action)
+        {
+        case eAction::AddElement:
+        {
+            int num;
+            input("Ievadiet skaitli: ", num);
+            stack.push(num);
+            break;
+        }
+        case eAction::PopElement:
+        {
+            cout << "Izņemts elements: " << stack.pop() << endl;
+            break;
+        }
+        case eAction::Empty:
+        {
+            if(stack.empty())
+                cout << "Steks ir tukšs." << endl;
+            else
+                cout << "Steks nav tukšs." << endl;
+            break;
+        }
+        case eAction::Exit:
+            return 0;
+        }
+    }
 }
