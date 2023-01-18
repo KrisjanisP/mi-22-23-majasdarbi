727c75c8d780d07a172b356df6de57299c40449c
KrisjanisP <krisjanispetrucena@gmail.com>
2023-01-18 13:55:06 +0200

    1-4

---
+++ ai/1-nodarbiba/1-4.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+def f(x):
+    return np.sin(2 * x) + 2 * np.e ** (3 * x)
+
+
+x_arr = np.linspace(start=-2, stop=2, num=1000)
+y_arr = f(x_arr)
+
+plt.plot(x_arr, y_arr)
+plt.show()
