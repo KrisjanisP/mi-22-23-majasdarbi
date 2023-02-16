351b4dc1ead969a09b01a724f8f44a45c3ed9aa3
KrisjanisP <krisjanispetrucena@gmail.com>
2023-02-16 22:05:15 +0200

    4-3

--- ai/4-nodarbiba/4-3.py
+++ ai/4-nodarbiba/4-3.py
@@ -25,47 +25,62 @@ def loss_mae(y_prim, y):
     return np.sum(np.abs(y_prim-y))
 
 def dW_linear(W, b, x):
-    return 0 #TODO
+    return x
 
 def db_linear(W, b, x):
-    return 0 #TODO
+    return 1
 
 def dx_linear(W, b, x):
-    return 0 #TODO
+    return W
 
 def dx_sigmoid(x):
-    return 0 #TODO
+    return np.exp(-x)/(1+np.exp(-x))**2
 
 def dy_prim_loss_mae(y_prim, y):
-    return 0 #TODO
+    return (y_prim-y)/(np.abs(y_prim-y)+1e-8)
 
 def dW_1_loss(x, W_1, b_1, W_2, b_2, y_prim, y):
-    return 0 #TODO
+    d_layer_1 = dW_linear(W_1, b_1, x)
+    d_layer_2 = dx_sigmoid(linear(W_1, b_1, x))
+    d_layer_3 = dx_linear(W_2, b_2, sigmoid(linear(W_1, b_1, x)))
+    d_loss = dy_prim_loss_mae(y_prim, y)
+    return d_loss*d_layer_3*d_layer_2*d_layer_1
 
 def db_1_loss(x, W_1, b_1, W_2, b_2, y_prim, y):
-    return 0 #TODO
+    d_layer_1 = db_linear(W_1, b_1, x)
+    d_layer_2 = dx_sigmoid(linear(W_1, b_1, x))
+    d_layer_3 = dx_linear(W_2, b_2, sigmoid(linear(W_1, b_1, x)))
+    d_loss = dy_prim_loss_mae(y_prim, y)
+    return d_loss*d_layer_3*d_layer_2*d_layer_1
 
 def dW_2_loss(x, W_1, b_1, W_2, b_2, y_prim, y):
-    return 0 #TODO
+    d_layer_3 = dW_linear(W_2, b_2, sigmoid(linear(W_1, b_1, x)))
+    d_loss = dy_prim_loss_mae(y_prim, y)
+    return d_loss*d_layer_3
 
 def db_2_loss(x, W_1, b_1, W_2, b_2, y_prim, y):
-    return 0 #TODO
+    d_layer_3 = db_linear(W_2, b_2, sigmoid(linear(W_1, b_1, x)))
+    d_loss = dy_prim_loss_mae(y_prim, y)
+    return d_loss*d_layer_3
 
 
-learning_rate = 1e-1
+learning_rate = 1e-4
 losses = []
-for epoch in range(1000):
+for epoch in range(1000000):
 
     Y_prim = model(X, W_1, b_1, W_2, b_2)
     loss = loss_mae(Y_prim, Y)
 
-    #TODO SGD
-    dW_1 = np.sum(dW_1_loss(X, W_1, b_1, W_2, b_2, Y_prim, y))
+    W_1 = W_1 - learning_rate * np.sum(dW_1_loss(X, W_1, b_1, W_2, b_2, Y_prim, Y))
+    b_1 = b_1 - learning_rate * np.sum(db_1_loss(X, W_1, b_1, W_2, b_2, Y_prim, Y))
+    W_2 = W_2 - learning_rate * np.sum(dW_2_loss(X, W_1, b_1, W_2, b_2, Y_prim, Y))
+    b_2 = b_2 - learning_rate * np.sum(db_2_loss(X, W_1, b_1, W_2, b_2, Y_prim, Y))
 
-    print(f'Y_prim: {Y_prim}')
-    print(f'Y: {Y}')
-    print(f'loss: {loss}')
     losses.append(loss)
+
+print(f'Y_prim: {Y_prim}')
+print(f'Y: {Y}')
+print(f'loss: {loss}')
 
 plt.plot(losses)
 plt.show()
